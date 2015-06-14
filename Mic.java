09747fc8ac68407f744a3567502f1acc155ebcf3
Pranab <islampranab@gmail.com>
2015-06-13 21:14:47 -0400

    Added  file stream and threads

--- Mic.java
+++ Mic.java
@@ -1,4 +1,5 @@
 import javax.sound.sampled*;
+import java.io.*;
 
 public class Mic{
     
@@ -10,6 +11,45 @@ public class Mic{
 	try {
 	    AudioFormat format = new AudioFormat(AudioFormat.Encoding.PCM_SIGNED, 44100, 16, 2, 4, 44100, false);
 
+	    DataLine.Info info = new DataLine.Info(TargetDataLine.class, format);
+	    if(!AudioSystem.isLineSupported(info))
+		{
+		    System.err.println("Line is not supported");
+		}
+
+	    final TargetDataLine targetLine = (TargetDataLine)AudioSystem.getLine(info); 
+	    targetLine.open();
+
+	    System.out.println("Recording voice");
+	    targetLine.start();
+
+	    Thread thread = new Thread();
+	    {
+		@Override public void run()
+		{
+		    AudioInputStream audioStream = new AudioInputStream(targetLine);
+		    File audioFile = new File("Recording.wav");
+		    try {AudioSystem.write(audioStream, AudioFileFormat.Type.WAVE, audioFile);}
+		    catch(IOException e){
+			e.printStackTrace();
+		    }
+		    System.out.println("Recording Stopped");
+		    
+		}
+	    };
+	    Thread.sleep(5000);
+	    targetLine.stop();
+	    targetLine.close();
+	}
+	catch(InterruptedException ie){
+	    ie.printStackTrace();
+	}
+	catch(LineUnavailableException e){
+	    e.printStackTrace();
+	}
+	catch(IOException ee){
+	    ee.printStackTrace();
+	}
     }
 
 
