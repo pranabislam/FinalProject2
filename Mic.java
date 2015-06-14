d09d7b684dae23c8ee826df7bfcbfe0e5181fd8c
Pranab <islampranab@gmail.com>
2015-06-13 20:47:11 -0400

    mic basic

---
+++ Mic.java
@@ -0,0 +1,17 @@
+import javax.sound.sampled*;
+
+public class Mic{
+    
+    
+    
+    public static void main(String[] args){
+	System.out.println("Sound test starting");
+	
+	try {
+	    AudioFormat format = new AudioFormat(AudioFormat.Encoding.PCM_SIGNED, 44100, 16, 2, 4, 44100, false);
+
+    }
+
+
+
+}
